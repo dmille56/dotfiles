b6fa67573b028d19237da7921e06821180492ef4
dmille56 <donovanm56@gmail.com>
2017-09-14 21:33:45 -0600

    fixing small issue

--- symlinkscript.sh
+++ symlinkscript.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-for file in $(find . -not -path "./git*" -not -path "./symlinkscript.sh" -not -path ".")
+for file in $(find . -not -path "./.git*" -not -path "./symlinkscript.sh" -not -path ".")
 do
 	trimmed_file=$(echo "$file" | cut -c 3-)
 	if [ -f "$trimmed_file" ]; then
 		#symlink the file
-		ln -f $trimmed_file ~/$trimmed-file
+		ln -f $trimmed_file ~/$trimmed_file
 	else
 		#make a directory for the directory structure
 		mkdir ~/$trimmed_file
