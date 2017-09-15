997717d5308315549b5cbcdaf32876388f363f54
dmille56 <donovanm56@gmail.com>
2017-09-14 21:30:51 -0600

    adding symlinkscript

---
+++ symlinkscript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for file in $(find . -not -path "./git*" -not -path "./symlinkscript.sh" -not -path ".")
+do
+	trimmed_file=$(echo "$file" | cut -c 3-)
+	if [ -f "$trimmed_file" ]; then
+		#symlink the file
+		ln -f $trimmed_file ~/$trimmed-file
+	else
+		#make a directory for the directory structure
+		mkdir ~/$trimmed_file
+	fi
+done
