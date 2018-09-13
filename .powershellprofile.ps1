df5c95ec4d4807102784dd15cfca3f7c99413823
dmille56 <donovanm56@gmail.com>
2018-09-12 23:07:21 -0700

    adding get-weather function for powershell profile

--- .powershellprofile.ps1
+++ .powershellprofile.ps1
@@ -9,7 +9,30 @@ function Prompt
     return " "
 }
 
-Set-PSReadLineKeyHandler -Chord 'Shift+Insert' -Function Paste
+function Get-Weather
+{
+	Param (
+		[Parameter(Mandatory = $false, Position = 0)]
+		[string]
+		$Location
+    )
 
+	(Invoke-WebRequest http://wttr.in/$Location -UserAgent curl).Content
+}
+
+Set-Alias weath Get-Weather
+
+Set-PSReadLineOption -EditMode Windows
+Set-PSReadLineKeyHandler -Chord Shift+Insert -Function Paste
+Set-PSReadLineKeyHandler -Chord Control+w -Function SelectAll
+Set-PSReadLineKeyHandler -Chord Control+d -Function MenuComplete
+
+# Remove if you don't have fzf
+Remove-PSReadLineKeyHandler -Chord Control+r
+Import-Module PSFzf
+
+# need to have this installed: Import-Module PowerShellCookbook
 Import-Module PSColor
 Import-Module Posh-Git
+
+Set-Alias ls gci
