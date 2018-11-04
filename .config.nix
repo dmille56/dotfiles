2b0f7d196ca39c75f7adea4163c7d1722afd7146
Donovan Miller <donovanm56@gmail.com>
2018-11-03 19:27:38 -0700

    updating nix configuration

--- .config.nix
+++ .config.nix
@@ -1,41 +1,55 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
 { config, pkgs, ... }:
 
 {
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
 
-  nixpkgs.config.pulseaudio = true;
+  nixpkgs.config.allowUnfree = true;
 
-  services.xserver = {
-    enable = true;
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  # boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # Define on which hard drive you want to install Grub.
+  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+  boot.loader.grub.useOSProber = true;
 
-    desktopManager.default = "none";
-    desktopManager.xterm.enable = false;
+  networking.hostName = "nixos"; # Define your hostname.
+  networking.networkmanager.enable = true;
 
-    displayManager.lightdm.enable = true;
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-    windowManager.default = "xmonad";
-    windowManager.xmonad.enable = true;
-  };
+  # Select internationalisation properties.
+  # i18n = {
+  #   consoleFont = "Lat2-Terminus16";
+  #   consoleKeyMap = "us";
+  #   defaultLocale = "en_US.UTF-8";
+  # };
 
+  # Set your time zone.
+  time.timeZone = "America/Los_Angeles";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    zsh
+    wget vim curl git emacs firefox zsh networkmanager xterm
+
     powershell
-
-    xterm
     tmux
-
-    curl
-    wget
-
     ranger
     fzf
 
-    rofi
-    dmenu
-
-    emacs
-    vim
-
-    git
+    chromium
 
     stack
 
@@ -43,12 +57,73 @@
     mpv
     cmus
 
-    firefox
-    chromium
+    rofi dmenu
+
+    haskellPackages.xmobar xscreensaver
+
+    gnome3.gnome-system-monitor
 
     powerline-fonts
   ];
 
-  time.timeZone = "America/Los_Angeles";
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+
+  # List services that you want to enable:
+  
+  services.nixosManual.showManual = true;
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.layout = "us";
+  services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable touchpad support.
+  services.xserver.libinput.enable = true;
+
+  # Enable the KDE Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  services.xserver.windowManager.xmonad.enable = true;
+  services.xserver.windowManager.xmonad.enableContribAndExtras = true;
+  services.xserver.desktopManager.xterm.enable = false;
+
+  programs.zsh.enable = true;
+  users.defaultUserShell = pkgs.zsh;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.dono = {
+    createHome = true;
+    group = "users";
+    extraGroups = ["wheel" "networkmanager" ];
+    home = "/home/dono";
+    useDefaultShell = true;
+    uid = 1000;
+  };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "18.09"; # Did you read the comment?
 
 }
