f8b18a122174da66f71c5216582b30f746b61652
dmille56 <donovanm56@gmail.com>
2018-12-07 19:45:54 -0800

    reorganizing xmonad config

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -14,13 +14,18 @@ import Graphics.X11.ExtraTypes.XF86
 myModMask = mod1Mask -- Bind Mod to the left alt key
 -- myModMask = mod4Mask -- Bind Mod to the windows key
 
-myTabConfig = def { inactiveBorderColor = "#928374"
-		    , inactiveColor = "#282828"
-	            , activeTextColor = "#689d6a"
-		    , activeBorderColor = "#689d6a"
-		    , activeColor = "#282828"
-	            , urgentTextColor = "red"
-	            , decoHeight = 20 }
+greyColor = "#928374"
+greyColor2 = "#282828"
+greenColor = "#689d6a"
+yellowColor = "#ebdbb2"
+
+myTabConfig = def { inactiveBorderColor = greyColor
+                    , inactiveColor = greyColor2
+                    , activeTextColor = greenColor
+                    , activeBorderColor = greenColor
+                    , activeColor = greyColor2
+                    , urgentTextColor = "red"
+                    , decoHeight = 20 }
 
 myLayout = (tabbed shrinkText myTabConfig) ||| tiled ||| Full
   where 
@@ -33,39 +38,45 @@ myManageHook = composeAll [
         className =? "spotify" --> doShift "3:music",
         className =? "pavucontrol" --> doShift "3:music",
         className =? "steam" --> doShift "4:games",
-	(isFullscreen --> doFullFloat),
-	manageDocks,
-	manageHook def
-	]
+        (isFullscreen --> doFullFloat),
+        manageDocks,
+        manageHook def
+        ]
+
+audioPlayPauseCommand = "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause"
+audioPreviousCommand = "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous"
+audioNextCommand = "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next"
+audioLowerVolumeCommand = "amixer -D pulse sset Master 5%-; notify-send -i audio-volume-medium -t 1000 'Volume: '$(amixer -D pulse sget Master | grep 'Left:' | awk -F'[][]' '{ print $2 } ')"
+audioRaiseVolumeCommand = "amixer -D pulse sset Master 5%+; notify-send -i audio-volume-medium -t 1000 'Volume: '$(amixer -D pulse sget Master | grep 'Left:' | awk -F'[][]' '{ print $2 } ')"
 
 main = do
     xmproc <- spawnPipe "xmobar"
 
     xmonad $ def
         { manageHook = myManageHook
-	, layoutHook = (smartBorders . avoidStruts) myLayout
+        , layoutHook = (smartBorders . avoidStruts) myLayout
         , logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "#ebdbb2" "" . shorten 50
-                        , ppCurrent = xmobarColor "#689d6a" ""
+                        , ppTitle = xmobarColor yellowColor "" . shorten 50
+                        , ppCurrent = xmobarColor greenColor ""
                         }
         , handleEventHook = handleEventHook def <+> XMonad.Hooks.EwmhDesktops.fullscreenEventHook <+> docksEventHook
         , modMask = myModMask
         , borderWidth = 2
-        , normalBorderColor = "#928374"
-        , focusedBorderColor = "#689d6a"
-	, workspaces = ["1:dev","2:web","3:music","4:games","5","6","7","8","9"]
+        , normalBorderColor = greyColor
+        , focusedBorderColor = greenColor
+        , workspaces = ["1:dev","2:web","3:music","4:games","5","6","7","8","9"]
         } `additionalKeys`
         [ ((myModMask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; xset dpms force off")
         , ((myModMask , xK_p), spawn "rofi -show run")
-        , ((0, xF86XK_AudioPlay), spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause")
-        , ((myModMask, xK_F12), spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause")
-        , ((0, xF86XK_AudioPrev), spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous")
-        , ((myModMask, xK_F10), spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous")
-        , ((0, xF86XK_AudioNext), spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next")
-        , ((myModMask, xK_F11), spawn "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next")
-        , ((0, xF86XK_AudioLowerVolume), spawn "amixer -D pulse sset Master 5%-; notify-send -i audio-volume-medium -t 1000 'Volume: '$(amixer -D pulse sget Master | grep 'Left:' | awk -F'[][]' '{ print $2 } ')")
-        , ((myModMask, xK_Down), spawn "amixer -D pulse sset Master 5%-; notify-send -i audio-volume-medium -t 1000 'Volume: '$(amixer -D pulse sget Master | grep 'Left:' | awk -F'[][]' '{ print $2 } ')")
-        , ((0, xF86XK_AudioRaiseVolume), spawn "amixer -D pulse sset Master 5%+; notify-send -i audio-volume-medium -t 1000 'Volume: '$(amixer -D pulse sget Master | grep 'Left:' | awk -F'[][]' '{ print $2 } ')")
-        , ((myModMask, xK_Up), spawn "amixer -D pulse sset Master 5%+; notify-send -i audio-volume-medium -t 1000 'Volume: '$(amixer -D pulse sget Master | grep 'Left:' | awk -F'[][]' '{ print $2 } ')")
+        , ((0, xF86XK_AudioPlay), spawn audioPlayPauseCommand)
+        , ((myModMask, xK_F12), spawn audioPlayPauseCommand)
+        , ((0, xF86XK_AudioPrev), spawn audioPreviousCommand)
+        , ((myModMask, xK_F10), spawn audioPreviousCommand)
+        , ((0, xF86XK_AudioNext), spawn audioNextCommand)
+        , ((myModMask, xK_F11), spawn audioNextCommand)
+        , ((0, xF86XK_AudioLowerVolume), spawn audioLowerVolumeCommand)
+        , ((myModMask, xK_Down), spawn audioLowerVolumeCommand)
+        , ((0, xF86XK_AudioRaiseVolume), spawn audioRaiseVolumeCommand)
+        , ((myModMask, xK_Up), spawn audioRaiseVolumeCommand)
         ]
