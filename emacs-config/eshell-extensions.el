45a9d290dbef73452f0e742142bb867892f29a70
Donovan Miller <donovanm56@gmail.com>
2024-01-22 12:01:42 -0800

    updating eshell-extensions.el

--- emacs-config/eshell-extensions.el
+++ emacs-config/eshell-extensions.el
@@ -6,21 +6,16 @@
 
 (require 'eshell-toggle)
 
-(defvar eshell-extensions-toggle-buffer-regex "\*et:.*")
-
-(defun find-buffer-regex (regex)
-  "Find the first buffer whose name matches the given REGEX."
-  (interactive "sBuffer name regex: ")
-  (catch 'found
-    (dolist (buf (buffer-list))
-      (when (string-match regex (buffer-name buf))
-        (throw 'found buf)))))
+(defun eshell-extensions-get-toggle-window (buf)
+  "Open BUF in another window if not already visible, otherwise focus on it."
+    (if (not buf)
+        (message "No buffer %s" buf)
+      (let ((win (get-buffer-window buf)))
+        (if win
+            (select-window win)
+          (eshell-toggle)))))
 
 (defun eshell-extensions-get-toggle-buffer ()
-  "Get the eshell toggle buffer."
-  (find-buffer-regex eshell-extensions-toggle-buffer-regex))
-
-(defun eshell-extensions-get-toggle-buffer-2 ()
   "Get the eshell toggle buffer."
   (let ((buf-name (eshell-toggle--make-buffer-name)))
     (unless (get-buffer buf-name)
@@ -33,6 +28,7 @@
 (defun eshell-extensions-send-string-to-eshell (command)
   "Send a COMMAND string to an Eshell buffer and execute it."
   (let ((eshell-buffer (eshell-extensions-get-toggle-buffer)))
+    (eshell-extensions-get-toggle-window eshell-buffer)
     ;; Send the command to Eshell
     (with-current-buffer eshell-buffer
       (goto-char (point-max))              ; Move to the end of the Eshell buffer
@@ -42,7 +38,8 @@
 ;; Usage example
 ;; (eshell-extensions-send-string-to-eshell "echo Hello, Eshell!")
 
-;; (print (eshell-extensions-get-toggle-buffer-2))
+;; (eshell-extensions-get-toggle-window (eshell-extensions-get-toggle-buffer-2))
+
 
 (provide 'eshell-extensions)
 ;;; eshell-extensions.el ends here
