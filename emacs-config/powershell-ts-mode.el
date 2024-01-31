68589f2162978b7c72d07684557b36c00ff76a72
Donovan Miller <donovanm56@gmail.com>
2024-01-30 18:39:42 -0800

    adding param_block indentation

--- emacs-config/powershell-ts-mode.el
+++ emacs-config/powershell-ts-mode.el
@@ -229,6 +229,7 @@ And not a class or function parent."
                  ((parent-is "class_method_definition") parent-bol powershell-ts-mode-indent-offset)
                  ((parent-is "function_statement") parent-bol powershell-ts-mode-indent-offset)
                  ((parent-is "ERROR") parent-bol powershell-ts-mode-indent-offset) ;; :NOTE: indent for params is weird here... not sure why the grammar parses it like this... might cause issues
+                 ((parent-is "param_block") parent-bol powershell-ts-mode-indent-offset)
                  (no-node parent 0)
                  )))
 
