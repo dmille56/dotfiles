9b6fd4d869cd383adf62ec57a0cd19e79be91ae3
Donovan Miller <donovanm56@gmail.com>
2024-01-30 18:51:59 -0800

    adding another indent rule

--- emacs-config/powershell-ts-mode.el
+++ emacs-config/powershell-ts-mode.el
@@ -231,6 +231,7 @@ And not a class or function parent."
                  ((parent-is "ERROR") parent-bol powershell-ts-mode-indent-offset) ;; :NOTE: indent for params is weird here... not sure why the grammar parses it like this... might cause issues
                  ((parent-is "param_block") parent-bol powershell-ts-mode-indent-offset)
                  ((parent-is "hash_literal_expression") parent-bol powershell-ts-mode-indent-offset)
+                 ((parent-is "array_expression") parent-bol powershell-ts-mode-indent-offset)
                  (no-node parent 0)
                  )))
 
