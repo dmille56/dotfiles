4541168f99ac3e1cc1949c2556abdbc0ccb917c0
Donovan Miller <donovanm56@gmail.com>
2024-02-09 01:07:28 -0800

    adding window indexing to start at 1 in tmux.confg

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -338,6 +338,10 @@ in {
         bind -n F6 select-pane -t :.+
 
         set -g mouse on
+
+        # Start windows and panes at 1, not 0
+        set -g base-index 1
+        setw -g pane-base-index 1
     '';
   };
 
