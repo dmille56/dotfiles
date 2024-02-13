fdb567ec72d8f5f48fa77581c3a67f9eed845c13
Donovan Miller <donovanm56@gmail.com>
2024-02-13 00:19:03 -0800

    updating emacs config

--- emacs-config/main.el
+++ emacs-config/main.el
@@ -338,6 +338,14 @@
 ;; :TODO: figure out how to use this package effectively
 (use-package evil-textobj-tree-sitter)
 
+;; bind `function.outer`(entire function block) to `f` for use in things like `vaf`, `yaf`
+(define-key evil-outer-text-objects-map "f" (evil-textobj-tree-sitter-get-textobj "function.outer"))
+;; bind `function.inner`(function block without name and args) to `f` for use in things like `vif`, `yif`
+(define-key evil-inner-text-objects-map "f" (evil-textobj-tree-sitter-get-textobj "function.inner"))
+
+;; You can also bind multiple items and we will match the first one we can find
+(define-key evil-outer-text-objects-map "a" (evil-textobj-tree-sitter-get-textobj ("conditional.outer" "loop.outer")))
+
 ;; Set default font
 (set-face-attribute 'default nil
                     :family "Deja Vu Sans Mono for Powerline"
