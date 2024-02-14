3be7f550c7527bf6b12443002859568bfd543b8c
Donovan Miller <donovanm56@gmail.com>
2024-02-14 13:23:17 -0800

    adding hooks for diff-hl to magit

--- emacs-config/main.el
+++ emacs-config/main.el
@@ -708,7 +708,10 @@
 
 (use-package diff-hl
   :init
-  (global-diff-hl-mode))
+  (global-diff-hl-mode)
+  (add-hook 'magit-pre-refresh-hook 'diff-hl-magit-pre-refresh)
+  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
+)
 
 (use-package read-aloud
   :init
