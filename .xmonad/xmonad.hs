02c85dd751cff02e27d6e4e1b19247b868561281
Donovan Miller <donovanm56@gmail.com>
2024-02-22 00:15:43 -0800

    updating xmonad config

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -24,6 +24,7 @@ import XMonad.Util.EZConfig (additionalKeys)
 import XMonad.Util.NamedScratchpad
 import XMonad.Util.Run (spawnPipe)
 
+myModMask :: KeyMask
 myModMask = mod4Mask -- Bind Mod to the windows key
 
 myTheme :: MyTheme
@@ -59,8 +60,8 @@ myManageHook =
       className =? "mpv" --> doShift "2:vid",
       className =? "vlc" --> doShift "2:vid",
       className =? "Chromium-browser" --> doShift "2:vid",
-      className =? "Emacs" --> doShift "5:dev",
-      className =? "Termonad-linux-x86_64" --> doShift "4:term",
+      className =? "Emacs" --> doShift "9:dev",
+      className =? "Termonad-linux-x86_64" --> doShift "8:term",
       title =? "Mozilla Firefox" --> doShift "1:web",
       (isFullscreen --> doFullFloat),
       manageDocks,
@@ -151,7 +152,7 @@ main = do
           borderWidth = 2,
           normalBorderColor = (_myTheme_normalBorderColor myTheme),
           focusedBorderColor = (_myTheme_focusedBorderColor myTheme),
-          workspaces = ["1:web", "2:vid", "3:game", "4:term", "5:dev", "6", "7", "8", "9"]
+          workspaces = ["1:web", "2:vid", "3:game", "4", "5", "6", "7", "8:term", "9:dev"]
         }
         `additionalKeys` [ ((myModMask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; xset dpms force off"),
                            ((myModMask, xK_p), spawn "rofi -show run"),
@@ -172,6 +173,8 @@ main = do
                            ((myModMask .|. shiftMask, xK_bracketright), openScratchPad "volumeControl"),
                            ((myModMask, xK_bracketleft), openScratchPad "music"),
                            ((myModMask .|. shiftMask, xK_bracketleft), openScratchPad "fileManager"),
+                           ((myModMask, xK_j), spawn "xdotool key Page_Down"), -- Remap mod+j, mod+k to page down / up
+                           ((myModMask, xK_k), spawn "xdotool key Page_Up"),
                            ((myModMask, xK_F9), spawn audioQueryTrackInfoCommand),
                            ((0, xF86XK_AudioPlay), spawn audioPlayPauseCommand),
                            ((myModMask, xK_F12), spawn audioPlayPauseCommand),
