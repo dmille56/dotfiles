3f7d192a2473feb7adf9be9c154ca1c72765aa45
Donovan Miller <donovanm56@gmail.com>
2024-11-01 14:01:10 -0700

    adding function to run a command using vterm to emacs

--- emacs-config/main.el
+++ emacs-config/main.el
@@ -1710,12 +1710,49 @@ Make sure to run \='ollama serve\=' and have zephyr model."
   (eaf-bind-key insert_or_open_link "a" eaf-browser-keybinding)
   (eaf-bind-key nil "M-q" eaf-browser-keybinding)) ;; unbind, see more in the Wiki
 
+(defun run-in-vterm-kill (process event)
+  "A process sentinel. Kills PROCESS's buffer if it is live."
+  (let ((b (process-buffer process)))
+    (and (buffer-live-p b)
+         (kill-buffer b))))
+
+(defun run-in-vterm (command)
+  "Execute string COMMAND in a new vterm.
+
+Interactively, prompt for COMMAND with the current buffer's file
+name supplied. When called from Dired, supply the name of the
+file at point.
+
+Like `async-shell-command`, but run in a vterm for full terminal features.
+
+The new vterm buffer is named in the form `*foo bar.baz*`, the
+command and its arguments in earmuffs.
+
+When the command terminates, the shell remains open, but when the
+shell exits, the buffer is killed."
+  (interactive
+   (list
+    (let* ((f (cond (buffer-file-name)
+                    ((eq major-mode 'dired-mode)
+                     (dired-get-filename nil t))))
+           (filename (concat " " (shell-quote-argument (and f (file-relative-name f))))))
+      (read-shell-command "Terminal command: "
+                          (cons filename 0)
+                          (cons 'shell-command-history 1)
+                          (list filename)))))
+  (with-current-buffer (vterm (concat "*" command "*"))
+    (set-process-sentinel vterm--process #'run-in-vterm-kill)
+    (vterm-send-string command)
+    (vterm-send-return)))
+
 (defun launch-lazygit ()
   "Launch lazygit in external terminal."
   (interactive)
-  (if (eq my/config-machine 'pc)
-      (ee-lazygit)
-    (start-process "lazygit-process" "*lazygit-buffer" "alacritty" "-e" "lazygit")) )
+  (cond
+   ((eq my/config-machine 'pc) (ee-lazygit))
+   ((eq my/config-machine 'work) (start-process "lazygit-process" "*lazygit-buffer" "alacritty" "-e" "lazygit"))
+   (t (run-in-vterm "lazygit"))
+  ))
 
 (use-package eee
   :straight (:host github :repo "eval-exec/eee.el" :files ("*.el" "*.sh"))
