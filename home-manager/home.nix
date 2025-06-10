ff978a20af39700ecc09b0e8a76b7197476b2c15
Donovan Miller <donovanm56@gmail.com>
2025-06-10 13:30:55 -0700

    home.nix updates

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: 
 
-# How to upgrade nix pkgs:
+## # How to upgrade nix pkgs:
 # nix-channel --update
 # nix-env --upgrade
 # home-manager switch --impure
@@ -81,7 +81,9 @@ in
     
     # fonts
     emacs-all-the-icons-fonts
-    nerd-fonts._3270 nerd-fonts.agave nerd-fonts.anonymice nerd-fonts.arimo nerd-fonts.aurulent-sans-mono nerd-fonts.bigblue-terminal nerd-fonts.bitstream-vera-sans-mono nerd-fonts.blex-mono nerd-fonts.caskaydia-cove nerd-fonts.caskaydia-mono nerd-fonts.code-new-roman nerd-fonts.comic-shanns-mono nerd-fonts.commit-mono nerd-fonts.cousine nerd-fonts.d2coding nerd-fonts.daddy-time-mono nerd-fonts.departure-mono nerd-fonts.dejavu-sans-mono nerd-fonts.droid-sans-mono nerd-fonts.envy-code-r nerd-fonts.fantasque-sans-mono nerd-fonts.fira-code nerd-fonts.fira-mono nerd-fonts.geist-mono nerd-fonts.go-mono nerd-fonts.gohufont nerd-fonts.hack nerd-fonts.hasklug nerd-fonts.heavy-data nerd-fonts.hurmit nerd-fonts.im-writing nerd-fonts.inconsolata nerd-fonts.inconsolata-go nerd-fonts.inconsolata-lgc nerd-fonts.intone-mono nerd-fonts.iosevka nerd-fonts.iosevka-term nerd-fonts.iosevka-term-slab nerd-fonts.jetbrains-mono nerd-fonts.lekton nerd-fonts.liberation nerd-fonts.lilex nerd-fonts.martian-mono nerd-fonts.meslo-lg nerd-fonts.monaspace nerd-fonts.monofur nerd-fonts.monoid nerd-fonts.mononoki nerd-fonts.mplus nerd-fonts.noto nerd-fonts.open-dyslexic nerd-fonts.overpass nerd-fonts.profont nerd-fonts.proggy-clean-tt nerd-fonts.recursive-mono nerd-fonts.roboto-mono nerd-fonts.shure-tech-mono nerd-fonts.sauce-code-pro nerd-fonts.space-mono nerd-fonts.symbols-only nerd-fonts.terminess-ttf nerd-fonts.tinos nerd-fonts.ubuntu nerd-fonts.ubuntu-mono nerd-fonts.ubuntu-sans nerd-fonts.victor-mono nerd-fonts.zed-mono # since can't install all nerdfonts in one go now
+    nerd-fonts._3270 nerd-fonts.agave nerd-fonts.anonymice nerd-fonts.arimo nerd-fonts.aurulent-sans-mono nerd-fonts.bigblue-terminal nerd-fonts.bitstream-vera-sans-mono nerd-fonts.blex-mono nerd-fonts.caskaydia-cove nerd-fonts.caskaydia-mono nerd-fonts.code-new-roman nerd-fonts.comic-shanns-mono nerd-fonts.commit-mono nerd-fonts.cousine nerd-fonts.d2coding nerd-fonts.daddy-time-mono nerd-fonts.departure-mono nerd-fonts.dejavu-sans-mono nerd-fonts.droid-sans-mono nerd-fonts.envy-code-r nerd-fonts.fantasque-sans-mono nerd-fonts.fira-code nerd-fonts.fira-mono nerd-fonts.geist-mono nerd-fonts.go-mono nerd-fonts.gohufont nerd-fonts.hack nerd-fonts.hasklug nerd-fonts.heavy-data nerd-fonts.hurmit nerd-fonts.im-writing nerd-fonts.inconsolata nerd-fonts.inconsolata-go nerd-fonts.inconsolata-lgc nerd-fonts.intone-mono nerd-fonts.iosevka nerd-fonts.iosevka-term nerd-fonts.iosevka-term-slab nerd-fonts.jetbrains-mono nerd-fonts.lekton nerd-fonts.liberation nerd-fonts.lilex nerd-fonts.martian-mono nerd-fonts.meslo-lg nerd-fonts.monaspace nerd-fonts.monofur nerd-fonts.monoid nerd-fonts.mononoki nerd-fonts.noto nerd-fonts.open-dyslexic nerd-fonts.overpass nerd-fonts.profont nerd-fonts.proggy-clean-tt nerd-fonts.recursive-mono nerd-fonts.roboto-mono nerd-fonts.shure-tech-mono nerd-fonts.sauce-code-pro nerd-fonts.space-mono nerd-fonts.symbols-only nerd-fonts.terminess-ttf nerd-fonts.tinos nerd-fonts.ubuntu nerd-fonts.ubuntu-mono nerd-fonts.ubuntu-sans nerd-fonts.victor-mono nerd-fonts.zed-mono # since can't install all nerdfonts in one go now
+    
+    #nerd-font.mplus
     
     godot_4-mono
 
@@ -148,7 +150,7 @@ in
     snowmachine
     lolcat
     ormolu
-    nixfmt
+    nixfmt-classic
     libvterm
     libtool
 
@@ -234,7 +236,7 @@ in
     enable = true;
     #autosuggestion.enable = true;
 
-    initExtra = ''
+    initContent = ''
      if [ -e ~/.nix-profile/etc/profile.d/nix.sh ]; then . ~/.nix-profile/etc/profile.d/nix.sh; fi
      source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
    '';
@@ -280,8 +282,6 @@ in
     enable = true;
     enableZshIntegration = true;
   };
-
-  programs.thefuck.enable = true;
 
   programs.rofi = {
     enable = true;
