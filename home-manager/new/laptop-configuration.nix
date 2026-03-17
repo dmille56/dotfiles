446ba313e673c4bad61b215392f068f9e7108a97
Donovan M <donovanm256@gmail.com>
2026-03-16 20:47:21 -0700

    .

--- home-manager/new/laptop-configuration.nix
+++ home-manager/new/laptop-configuration.nix
@@ -14,7 +14,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # networking.hostName = "nixos"; # Define your hostname. # :TODO: fix this if you need to
+  networking.hostName = "nixos"; # Define your hostname.
   networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
