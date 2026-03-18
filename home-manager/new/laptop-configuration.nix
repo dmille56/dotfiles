b17c5fa44fef8381b835a7374e01d6076818d88c
Donovan M <donovanm256@gmail.com>
2026-03-18 04:05:12 -0700

    .

--- home-manager/new/laptop-configuration.nix
+++ home-manager/new/laptop-configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
@@ -17,30 +13,5 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # List services that you want to enable:
-
-  # :TODO: enable an x11 vnc server
-  services.x2goserver.enable = true;
-
-  # Enable the OpenSSH daemon.
-  # :TODO: get the ssh enable correctly for both SSH and VNC (through ssh tunnel)
-  services.openssh.enable = true;
-  services.openssh.settings.X11Forwarding = true;
-  services.openssh.settings.PermitRootLogin = "no";
-
-  services.sshguard.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
+  services.openssh.enable = false;
 }
