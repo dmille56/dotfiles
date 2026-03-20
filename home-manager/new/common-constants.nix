27bac2a5f5bfb61b391d20b20896345a24f97d29
Donovan M <donovanm256@gmail.com>
2026-03-20 01:55:09 -0700

    .

--- home-manager/new/common-constants.nix
+++ home-manager/new/common-constants.nix
@@ -3,7 +3,8 @@ rec {
   my-home-dir = "/home/${my-username}";
   my-dotfile-dir = "${my-home-dir}/dotfiles";
   my-dotfile-nix-dir = "${my-dotfile-dir}/home-manager/new";
-  my-desktop-hostname = "castle";
-  # my-desktop-hostname = "nixos";
+  # :TODO: make the hostname to castle on desktop
+  # my-desktop-hostname = "castle";
+  my-desktop-hostname = "nixos";
   my-laptop-hostname = "vladtop";
 }
