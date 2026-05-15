834fd0158a8b4291198b79471b5cd2925be4739e
Donovan M <donovanm56@gmail.com>
2026-05-15 16:35:57 -0700

    update my-tts

--- nix/my-tts.nix
+++ nix/my-tts.nix
@@ -5,9 +5,9 @@ pkgs.writeShellScriptBin "my-tts" ''
   if read -t 0; then
       # handle input from stdin
       input=$(cat)
-      echo "$input" | piper --model ~/piper/models/en_US-lessac-high.onnx --output-raw | aplay -r 22050 -f S16_LE -t raw -
+      echo "$input" | piper --model ~/piper/models/en_GB-alan-low.onnx --output-raw | aplay -r 22050 -f S16_LE -t raw -
   else
       # handle input from an argument
-      echo $@ | piper --model ~/piper/models/en_US-lessac-high.onnx --output-raw | aplay -r 22050 -f S16_LE -t raw -
+      echo $@ | piper --model ~/piper/models/en_GB-alan-low.onnx --output-raw | aplay -r 22050 -f S16_LE -t raw -
   fi
 ''
