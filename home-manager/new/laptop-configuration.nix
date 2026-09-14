c4187f23f5f89f8b496a73938aba6d9a1ad8f0c7
Donovan M <donovanm56@gmail.com>
2026-09-13 20:12:20 -0700

    .

--- home-manager/new/laptop-configuration.nix
+++ home-manager/new/laptop-configuration.nix
@@ -12,6 +12,11 @@ in
 
   networking.hostName = "${constants.my-laptop-hostname}"; # Define your hostname.
 
+  # The ThinkPad Hybrid USB-C dock uses DisplayLink for its monitor outputs.
+  # Keep modesetting for the laptop's Intel GPU and add the DisplayLink driver
+  # for the dock's USB graphics adapter.
+  services.xserver.videoDrivers = [ "modesetting" "displaylink" ];
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
